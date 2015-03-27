b2b64cc0421085af2d40ee41a79525aed2b28be7
Lux0r <Lux0r@web.de>
2015-03-27 16:26:51 +0100

    Added function 'connectedToBase'
    Added a function to check if the given sector is connected with the
    corresponding base.

--- TacticalTeamCombat.Altis/SOS/TacticalTeamCombat/TTC_CTI/Locations/Patterns/9_sectors.hpp
+++ TacticalTeamCombat.Altis/SOS/TacticalTeamCombat/TTC_CTI/Locations/Patterns/9_sectors.hpp
@@ -1,16 +1,17 @@
 /*
 	Defines the neighbours of all sectors for the following concept:
 	https://cloud.githubusercontent.com/assets/11304010/6790532/8435d574-d1a8-11e4-840e-be4ac179693c.jpg
-	The numbers for the neighbours represent the index in the TTC_CTI_sectors array.
+
+	- The numbers for the neighbours represent the index in the TTC_CTI_sectors array.
+	- A string is used for each base: "base" + <side>, e.g.: "baseWEST"
 
 	Created by Lux0r
 */
 
-#include "baseIndexes.hpp"
 
 #define TTC_CTI_sectorPattern "9 sectors pattern (default)"
 
-#define TTC_CTI_neighbours_sector1 [TTC_CTI_baseWest, 1, 2]
+#define TTC_CTI_neighbours_sector1 ["baseWEST", 1, 2]
 #define TTC_CTI_neighbours_sector2 [0, 3, 4]
 #define TTC_CTI_neighbours_sector3 [0, 4, 5]
 #define TTC_CTI_neighbours_sector4 [1, 4, 6]
@@ -18,4 +19,4 @@
 #define TTC_CTI_neighbours_sector6 [2, 4, 7]
 #define TTC_CTI_neighbours_sector7 [3, 4, 8]
 #define TTC_CTI_neighbours_sector8 [4, 5, 8]
-#define TTC_CTI_neighbours_sector9 [6, 7, TTC_CTI_baseGuer]
+#define TTC_CTI_neighbours_sector9 [6, 7, "baseGUER"]
