8f2c791a5ca4565944cfa2f4c17da17d5628510b
Lux0r <Lux0r@web.de>
2015-04-07 13:32:52 +0200

    Weather #62
    Adjusted the weather mission parameters.

--- TacticalTeamCombat.Altis/SOS/Weather/params.hpp
+++ TacticalTeamCombat.Altis/SOS/Weather/params.hpp
@@ -1,20 +1,20 @@
 class Weather_Overcast {
 	title		= "Weather: Overcast (100% means storms and rain are very likely)";
-	values[]	= {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-	texts[]		= {"0%", "10% (Default)", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
-	default		= 10;
+	values[]	= {-1, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+	texts[]		= {"Random (Default)", "0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
+	default		= -1;
 };
 
 class Weather_Fog {
 	title		= "Weather: Fog";
-	values[]	= {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-	texts[]		= {"0% (Default)", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
-	default		= 0;
+	values[]	= {-1, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+	texts[]		= {"Random (Default)", "0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
+	default		= -1;
 };
 
 class Weather_Rain {
 	title		= "Weather: Rain (is not possible when overcast is less than 70%)";
-	values[]	= {0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-	texts[]		= {"0% (Default)", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
-	default		= 0;
+	values[]	= {-1, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+	texts[]		= {"Random (Default)", "0%", "10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"};
+	default		= -1;
 };
--- TacticalTeamCombat.Altis/SOS/Weather/setWeather.sqf
+++ TacticalTeamCombat.Altis/SOS/Weather/setWeather.sqf
@@ -4,15 +4,40 @@
 	Created by Lux0r
 */
 
+#define OVERCAST_MIN 0
+#define OVERCAST_MAX 70
+#define FOG_MIN 0
+#define FOG_MAX 30
+#define RAIN_MIN 0
+#define RAIN_MAX 50
+
+private ["_overcast","_fog","_rain"];
+
+
+_WEATHER_fnc_getParamValue = {
+	private ["_class","_min","_max","_param"];
+
+	_class	= _this select 0;
+	_min	= _this select 1;
+	_max	= _this select 2;
+	_param	= [_class, -1] call BIS_fnc_getParamValue;
+
+	if (_param == -1) then {
+		_param = [_min, _max] call BIS_fnc_randomInt;
+	};
+
+	_param;
+};
 
 // Run this script only on server-side.
 if (isServer) then {
-	_weather_overcast	= ["Weather_Overcast", 10] call BIS_fnc_getParamValue;
-	_weather_fog		= ["Weather_Fog", 0] call BIS_fnc_getParamValue;
-	_weather_rain		= ["Weather_Rain", 0] call BIS_fnc_getParamValue;
+	_overcast	= ["Weather_Overcast", OVERCAST_MIN, OVERCAST_MAX] call _WEATHER_fnc_getParamValue;
+	_fog		= ["Weather_Fog", FOG_MIN, FOG_MAX] call _WEATHER_fnc_getParamValue;
+	_rain		= ["Weather_Rain", RAIN_MIN, RAIN_MAX] call _WEATHER_fnc_getParamValue;
+	//diag_log format["WEATER: _overcast = %1, _fog = %2, _rain = %3", _overcast, _fog, _rain];
 
-	0 setOvercast (_weather_overcast / 100);
-	0 setFog (_weather_fog / 100);
-	0 setRain (_weather_rain / 100);
+	0 setOvercast (_overcast / 100);
+	0 setFog (_fog / 100);
+	0 setRain (_rain / 100);
 	forceWeatherChange;
 };
